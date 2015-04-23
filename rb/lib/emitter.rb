00fe62757131ed7597adcc540139ab5787d91aff
Stephen Caudill <scaudill@gmail.com>
2015-04-22 20:34:19 -0700

    Emitter can spit super region into the template now
    There's some light object affinity in the attribute handling. Should
    probably be encapsulated....

--- rb/lib/emitter.rb
+++ rb/lib/emitter.rb
@@ -1,26 +1,46 @@
 require 'erubis'
 require 'fileutils'
+require_relative './taxonomy'
 
 class Emitter
   attr_accessor :template
   attr_accessor :build_dir
+  attr_accessor :taxonomy
+
+  def self.file_name(region)
+    "#{region.downcase.gsub(/\W/,'_')}.html" if region
+  end
 
   def initialize(build_dir)
     self.template = Erubis::Eruby.new(File.read('template/destination.html.erb'))
     self.build_dir = build_dir
+    self.taxonomy = Taxonomy.new('../resources/taxonomy.xml')
     prepare_build_dir
   end
 
-  def call(attributes)
-    File.open("#{File.join(build_dir, title_file_name(attributes))}.html", 'w+') do |f|
-      f.write(template.result(:attrs => attributes))
+  def call(attrs)
+    File.open(File.join(build_dir, title_file_name(attrs)), 'w+') do |f|
+      f.write(template.result(template_attrs(attrs)))
     end
   end
 
   private
 
-  def title_file_name(attributes)
-    attributes[:destination][:title].downcase.gsub(/\W/,'_')
+  def template_attrs(attrs)
+    super_region = taxonomy.find(title(attrs)).ancestor
+    { region: title(attrs),
+      super_region: {
+        name: super_region,
+        file: self.class::file_name(super_region) },
+      description: attrs[:destination][:introductory][:introduction][:overview] }
+  end
+
+  def title(attrs)
+    attrs[:destination][:title]
+  end
+
+  def title_file_name(attrs)
+    self.class::file_name(title(attrs))
   end
 
   def prepare_build_dir
