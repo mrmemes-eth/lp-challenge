f5675450b97751026e2499e46f7c7d9afdcff0c3
Stephen Caudill <scaudill@gmail.com>
2015-04-23 14:43:21 -0600

    Implement #get_in on Hash

--- rb/lib/hash_ext.rb
+++ rb/lib/hash_ext.rb
@@ -8,5 +8,13 @@ class Hash
     trgt[last] = value.respond_to?(:call) ? value.call(trgt[last]) : value
     self
   end
+
+  def get_in(*path)
+    keys = path.dup
+    last = keys.pop
+    keys.reduce(self) do |acc,key|
+      acc.fetch(key,{})
+    end[last]
+  end
 end
 
--- rb/spec/hash_ext_spec.rb
+++ rb/spec/hash_ext_spec.rb
@@ -1,70 +1,99 @@
 require 'spec_helper'
 require_relative '../lib/hash_ext.rb'
 
-RSpec.describe Hash, "#update_in" do
+RSpec.describe Hash do
+  describe "#update_in" do
 
-  context "top level value, empty hash" do
-    let(:h){ Hash.new }
-    specify{ expect(h.update_in([:a],:b)).to eq({:a => :b}) }
-  end
-
-  context "top level value, non-empty hash" do
-    let(:h){ {:b => :c} }
-    specify{ expect(h.update_in([:a],:b)).to eq({:a => :b, :b => :c}) }
-  end
-
-  context "nested value" do
-    let(:h) { {:a => {:b => :c}} }
-    specify do
-      expect(h.update_in([:a,:c], :d)).to eq({:a => {:b => :c, :c => :d}})
+    context "top level value, empty hash" do
+      let(:h){ Hash.new }
+      specify{ expect(h.update_in([:a],:b)).to eq({:a => :b}) }
     end
-  end
 
-  context "arbitrarily deeply nested value" do
+    context "top level value, non-empty hash" do
+      let(:h){ {:b => :c} }
+      specify{ expect(h.update_in([:a],:b)).to eq({:a => :b, :b => :c}) }
+    end
+
+    context "nested value" do
+      let(:h) { {:a => {:b => :c}} }
+      specify do
+        expect(h.update_in([:a,:c], :d)).to eq({:a => {:b => :c, :c => :d}})
+      end
+    end
+
+    context "arbitrarily deeply nested value" do
+      let(:h) do
+        {:a => {:b => {:c => {:d => {:e => :f}}}},
+         :b => {:c => :d}}
+      end
+      let(:result) do
+        {:a => {:b => {:c => {:d => {:e => :g}}}},
+         :b => {:c => :d}}
+      end
+      specify do
+        expect(h.update_in([:a,:b,:c,:d,:e], :g)).to eq(result)
+      end
+    end
+
+    context "creates missing values as maps" do
+      let(:h) { Hash.new }
+      specify do
+        expect(h.update_in([:a],:b)).to eq({:a => :b})
+      end
+      specify do
+        expect(h.update_in([:a, :b],:c)).to eq({:a  => {:b => :c}})
+      end
+    end
+
+    context "when provided a lambda" do
+      let(:h) { {:a => {:b => :c}} }
+
+      context "simple lambda" do
+        let(:str_proc) do
+          ->(current) { current.to_s }
+        end
+        specify do
+          expect(h.update_in([:a,:b],str_proc)).to eq({:a => {:b => "c"}})
+        end
+      end
+
+      context "collating lambda" do
+        let(:update_proc) do
+          ->(new,current) { current ? [new].unshift(current) : new }
+        end
+        let(:result) { {:a => {:b => [:c,:d]}} }
+        specify do
+          expect(h.update_in([:a,:b], update_proc.curry.call(:d))).to eq(result)
+        end
+      end
+    end
+
+  end
+  
+  describe "#get_in" do
     let(:h) do
       {:a => {:b => {:c => {:d => {:e => :f}}}},
        :b => {:c => :d}}
     end
-    let(:result) do
-      {:a => {:b => {:c => {:d => {:e => :g}}}},
-       :b => {:c => :d}}
-    end
-    specify do
-      expect(h.update_in([:a,:b,:c,:d,:e], :g)).to eq(result)
-    end
-  end
 
-  context "creates missing values as maps" do
-    let(:h) { Hash.new }
-    specify do
-      expect(h.update_in([:a],:b)).to eq({:a => :b})
-    end
-    specify do
-      expect(h.update_in([:a, :b],:c)).to eq({:a  => {:b => :c}})
-    end
-  end
-
-  context "when provided a lambda" do
-    let(:h) { {:a => {:b => :c}} }
-
-    context "simple lambda" do
-      let(:str_proc) do
-        ->(current) { current.to_s }
-      end
+    context "shallow fetches" do
+      specify { expect(h.get_in(:b)).to eq({:c => :d}) }
       specify do
-        expect(h.update_in([:a,:b],str_proc)).to eq({:a => {:b => "c"}})
+        expect(h.get_in(:a, :b)).to eq({:c => {:d => {:e => :f}}})
+      end
+      context "when there is no key" do
+        specify { expect(h.get_in(:z)).to eq(nil) }
+        specify { expect(h.get_in(:a, :f)).to eq(nil) }
       end
     end
 
-    context "collating lambda" do
-      let(:update_proc) do
-        ->(new,current) { current ? [new].unshift(current) : new }
-      end
-      let(:result) { {:a => {:b => [:c,:d]}} }
+    context "arbitrarily deeply nested" do
       specify do
-        expect(h.update_in([:a,:b], update_proc.curry.call(:d))).to eq(result)
+        expect(h.get_in(:a,:b,:c,:d,:e)).to eq(:f)
+      end
+      context "when there is no key" do
+        specify { expect(h.get_in(:a,:b,:c,:d,:f)).to eq(nil) }
       end
     end
   end
-  
 end
